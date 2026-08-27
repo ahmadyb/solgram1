ffa5f865047db1c2d65ac537c1c8a781d9d3e94e
ahmadyb <114798730+ahmadyb@users.noreply.github.com>
2026-08-27 08:04:37 +0000

    Fix Kotlin 2.0 Compose plugin - add org.jetbrains.kotlin.plugin.compose
    Required since Kotlin 2.0.0-RC2 to use Compose Multiplatform.
    Fixes: Configuration problem: Since Kotlin 2.0.0-RC2 to use Compose Multiplatform you must apply org.jetbrains.kotlin.plugin.compose plugin.
    
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 plugins {
     kotlin("jvm") version "2.0.20"
     kotlin("plugin.serialization") version "2.0.20"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
     id("app.cash.sqldelight") version "2.0.2"
     id("org.jetbrains.compose") version "1.7.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.6"
